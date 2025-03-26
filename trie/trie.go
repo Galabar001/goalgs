939e563b8a53888553ec68550f25ae564360f49b
Kevin Regan <kevin.regan@salesforce.com>
2025-03-26 14:25:46 -0700

    Trie implementation.

--- trie/trie.go
+++ trie/trie.go
@@ -1,1 +1,38 @@
 package trie
+
+type UTF8Slice []byte
+
+type Trie[T any] interface {
+	Add(key UTF8Slice, value T) bool
+	Remove(key UTF8Slice) bool
+	Get(key UTF8Slice) (T, bool)
+	Clear()
+}
+
+type trieNode struct {
+}
+
+type trie[T any] struct {
+	root *trieNode
+}
+
+func New[T any]() Trie[T] {
+	return &trie[T]{}
+}
+
+func (t *trie[T]) Add(key UTF8Slice, value T) bool {
+	return false
+}
+
+func (t *trie[T]) Remove(key UTF8Slice) bool {
+	return false
+}
+
+func (t *trie[T]) Get(key UTF8Slice) (T, bool) {
+	var value T
+	return value, false
+}
+
+func (t *trie[T]) Clear() {
+	t.root = nil
+}
--- trie/trie_test.go
+++ trie/trie_test.go
@@ -2,8 +2,32 @@ package trie
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
-func TestNew(_ *testing.T) {
+func TestNew(t *testing.T) {
+	tr := New[int]()
+	require.NotNil(t, tr)
+}
 
+func TestAdd(_ *testing.T) {
+
+}
+
+func TestRemove(t *testing.T) {
+	tr := New[int]()
+	found := tr.Remove([]byte("the_key"))
+	require.False(t, found)
+}
+
+func TestGet(t *testing.T) {
+	tr := New[int]()
+	_, found := tr.Get([]byte("the_key"))
+	require.False(t, found)
+}
+
+func TestClear(_ *testing.T) {
+	tr := New[int]()
+	tr.Clear()
 }
